138e32e352d1d1170be06650b5703b31be2ddbce
mayi140611 <yongguiluo@hotmail.com>
2022-10-24 15:20:41 +0800

    sm

--- build_medicalgraph.py
+++ build_medicalgraph.py
@@ -12,11 +12,16 @@ class MedicalGraph:
     def __init__(self):
         cur_dir = '/'.join(os.path.abspath(__file__).split('/')[:-1])
         self.data_path = os.path.join(cur_dir, 'data/medical.json')
-        self.g = Graph(
-            host="127.0.0.1",  # neo4j 搭载服务器的ip地址，ifconfig可获取到
-            http_port=7474,  # neo4j 服务器监听的端口号
-            user="lhy",  # 数据库user name，如果没有更改过，应该是neo4j
-            password="lhy123")
+        neo4j_url = "bolt://10.89.23.188:7687"
+        neo4j_user = 'neo4j'
+        neo4j_pw = '123456'
+        graph = Graph(neo4j_url, auth=(neo4j_user, neo4j_pw))
+        self.g = graph
+#         self.g = Graph(
+#             host="127.0.0.1",  # neo4j 搭载服务器的ip地址，ifconfig可获取到
+#             http_port=7474,  # neo4j 服务器监听的端口号
+#             user="neo4j",  # 数据库user name，如果没有更改过，应该是neo4j
+#             password="123456")
 
     '''读取文件'''
     def read_nodes(self):
